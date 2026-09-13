7d7b7a2c5ffbfbf841aec8e2e496cd828b2ca226
gpt510222 <gpt510222@gmail.com>
2026-09-13 10:26:58 +0800

    Sync restored immortal layouts and scrolling

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const $ = s => document.querySelector(s);
 const $$ = s => [...document.querySelectorAll(s)];
-window.WENDAO_BUILD='20260913-89';
+window.WENDAO_BUILD='20260913-90';
 const qStyleMode=true;
 const formalImmortalRealmEnabled=false;
 const leaderboardConfig={url:'https://oxzuunzhsbvumxxbezev.supabase.co',publishableKey:'sb_publishable_u2rmM6v1-AdjRLMZSVetRw_MgjeWSL3',sessionKey:'wendao-supabase-session-release-v1',gameVersion:'v1.0.0',limit:50};
