a1b36ed8e3957b922ecd5cd913f58f74be765258
gpt510222 <gpt510222@gmail.com>
2026-09-15 16:05:46 +0800

    Number Cangji games

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const $ = s => document.querySelector(s);
 const $$ = s => [...document.querySelectorAll(s)];
-window.WENDAO_BUILD='20260915-131';
+window.WENDAO_BUILD='20260915-133';
 const qStyleMode=true;
 const formalImmortalRealmEnabled=true;
 const leaderboardConfig={url:'https://oxzuunzhsbvumxxbezev.supabase.co',publishableKey:'sb_publishable_u2rmM6v1-AdjRLMZSVetRw_MgjeWSL3',sessionKey:'wendao-supabase-session-release-v1',gameVersion:'v1.0.0',limit:50};
